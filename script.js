ada166836fc96dc3e27b25be7458e9a4f103d19d
fishbigger <toby.e.m.johnson@gmail.com>
2023-05-01 21:25:13 +0100

    Added basic error checking and country -> code

--- script.js
+++ script.js
@@ -1,19 +1,58 @@
+function errorCheck() {
+  code = document.getElementById('code').value;
+  country = document.getElementById('country').value;
+
+  error = document.getElementById('error');
+
+  error.textContent = '';
+
+  if (code == country && country == '') {
+    error.textContent = "Please enter either country name or code.";
+    return true
+  }
+
+  if (code != '' && country != '') {
+    error.textContent = "Please only enter either country name or code.";
+    return true
+  }
+
+  return false
+}
+
 function checkCode(e) {
   e.preventDefault()
 
-  code = document.getElementById('code').value;
+  if (errorCheck()) {
+    return
+  }
   result = document.getElementById('result');
+
+  code = document.getElementById('code').value;
+  country = document.getElementById('country').value;
 
   fetch("./countryCodes.json")
     .then((res) => res.json())
     .then((data) => {
-        country = data[code.toUpperCase()];
+        if (code) {
+          country = data[code.toUpperCase()];
 
-        if (country == undefined) {
-          result.textContent = "Unknown"
+          if (country == undefined) {
+            result.textContent = "Unknown"
+          } else {
+             result.textContent = country;
+          }
         } else {
-           result.textContent = country;
+          var found;
+          for (var i in data) {
+            if (data[i].toUpperCase() == country.toUpperCase()) {
+              result.textContent = "The country code for " + data[i] + " is " + i;
+              found = true;
+              break
+            }
+          }
+          if (!found) {
+            result.textContent = "Unknown"
+          }
         }
-
     });
 }
